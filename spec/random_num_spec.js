662f6197ace8e45fbabd6008e32180e7a9f31af2
aaa <1143581723@qq.com>
2013-09-29 16:39:35 +0800

    测试4个随机数互不相同

--- spec/random_num_spec.js
+++ spec/random_num_spec.js
@@ -15,6 +15,17 @@ describe("Given a random function when it working results random numbers ",funct
         var rightProb=count_prob(per_chance,1000);
         expect(rightProb).toBe(10);
     });
+    it("Given a random number when every number compared results 0", function () {
+        var random_number = createRandomNum();
+        var Tag=false;
+        for(var i=0;i<4;i++){
+            for(var j=i+1;j<4;j++){
+                if(random_number[i]===random_number[j])
+                    true;
+            }
+        }
+        expect(Tag).toBe(false);
+    });
 });
 
 
